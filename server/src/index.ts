68900f14d1308de131e8e7766ff663d08e4370dd
John Mwihaki <jmwkimani@gmail.com>
2025-07-30 02:03:03 +0300

    updates

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,20 +4,21 @@ import dotenv from "dotenv";
 import authRoutes from "./routes/auth.routes";
 import userRoutes from "./routes/user.routes";
 import taskRoutes from "./routes/tasks.route";
-dotenv.config(
-  
-);
+dotenv.config();
+
 const tasky = express();
-tasky.get("/", (_req:Request,res:Response)=>
-  res.status(200).json({messege:"Thank you .I got the test"})
-)
-tasky.use(cors(
-  {
-    origin:['https://i-taskify-app.vercel.app', 'http://localhost:5173'] ,
-    credentials:true,
-  }
-));
+
 tasky.use(express.json());
+
+tasky.get("/", (_req: Request, res: Response) =>
+  res.status(200).json({ messege: "Thank you .I got the test" })
+);
+tasky.use(
+  cors({
+    origin: ["https://i-taskify-app.vercel.app", "http://localhost:5173"],
+    credentials: true,
+  })
+);
 tasky.post("/", (_req, res) => {
   res.send("<h1>Welcome To The iTaskify manager app</h1>");
 });
